c69a54125cdae7d8aaa5022e34da1442b4152c19
tae0y <tae0yp@gmail.com>
2024-08-03 22:18:02 +0900

    frontend 다른점 없애기

--- frontend/AspireYouTubeSummariser.WebApp/Program.cs
+++ frontend/AspireYouTubeSummariser.WebApp/Program.cs
@@ -15,9 +15,6 @@ builder.Services.AddHttpClient<IApiAppClient, ApiAppClient>(p => p.BaseAddress =
 
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
-app.UseOutputCache(); // 이 구문을 추가해서 레디스 캐시 사용
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -32,4 +29,6 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+app.MapDefaultEndpoints();
+app.UseOutputCache(); // 이 구문을 추가해서 레디스 캐시 사용
 app.Run();
